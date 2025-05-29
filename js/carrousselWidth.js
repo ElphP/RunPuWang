9c1e48a737b0fc090ac171f8b8120668cfaf2695
Elphège <elphege78@gmail.com>
2025-05-29 16:39:26 +0200

    Version 1

---
+++ js/carrousselWidth.js
@@ -0,0 +1,11 @@
+const wrapper = document.getElementById("carousel-wrapper");
+const carousel = document.getElementById("carousel");
+
+// Mesurer les dimensions
+const distanceToScroll = carousel.scrollWidth - wrapper.clientWidth;
+
+// Injecter la largeur réelle dans une variable CSS
+wrapper.style.setProperty("--carousel-width", `${distanceToScroll}px`);
+
+const duration = distanceToScroll / 40; // diviseur + grand = vitesse + rapide
+wrapper.style.setProperty("--scroll-duration", `${duration}s`);
